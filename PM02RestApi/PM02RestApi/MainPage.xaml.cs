8db127632467ea2dac243e59e802f1d2e0136ccd
Jorge-Asfura77 <91570632+Jorge-Asfura77@users.noreply.github.com>
2022-11-13 21:46:17 -0600

    Agregar archivos de proyecto.

---
+++ PM02RestApi/PM02RestApi/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using PM02RestApi.Controllers;
+using PM02RestApi.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PM02RestApi
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
